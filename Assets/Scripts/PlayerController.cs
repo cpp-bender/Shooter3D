4f440b4cb2ce61a525ac351c089d1c6813059d73
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-03-17 12:22:39 +0300

    added(PlayerController,PlayerMovementData)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private Rigidbody body;
+    [SerializeField] private PlayerMovementData playerData;
+    private Vector3 moveVelocity;
+
+    [Header("Mouse Look Settings")]
+    [SerializeField] private Camera mainCamera;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        body.MovePosition(body.position + moveVelocity * playerData.MoveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Rotate()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+        if (ground.Raycast(ray, out rayDistance))
+        {
+            Vector3 rayPoint = ray.GetPoint(rayDistance);
+            Vector3 lookPoint = new Vector3(rayPoint.x, transform.position.y, rayPoint.z);
+            transform.LookAt(lookPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovementData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "TopDown Shooter/Player Movement")]
+public class PlayerMovementData : ScriptableObject
+{
+    [SerializeField] private float moveSpeed;
+
+    public float MoveSpeed { get => moveSpeed; }
+}
